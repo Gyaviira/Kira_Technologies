69a818eac61cb02e34a373e27fa1f0aa4dba8070
gyaviira <mayanja12079@gmail.com>
2023-11-13 00:23:03 +0300

    init

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, HttpResponseRedirect
+from django.urls import reverse
+from django.template.loader import render_to_string
 
-# Create your views here.
+def index(request):
+    return HttpResponse("This works!!")
